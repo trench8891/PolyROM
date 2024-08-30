03a53d8e4511431615c039a6953ff798e79d5906
trench8891 <trench8891@users.noreply.github.com>
2024-08-30 11:35:27 -0400

    Automate release (#15)

---
+++ bin/pack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# remove dev-only files
+rm -rf LE3/DLC_MOD_PolyROM/CookedPCConsole/Default_DLC_MOD_PolyROM
+
+# create archive directory and move mod files
+mkdir "PolyRomanceMod"
+mv LE3/ PolyRomanceMod/
+
+# compress files
+tar -cavf PolyRomanceMod.rar PolyRomanceMod/
